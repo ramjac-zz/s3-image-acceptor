aad0a12c1b6c394ecd3f4b724ee79d49bd668d31
Grady Fields <ramjacvp@gmail.com>
2018-12-03 22:09:34 -0500

    Fixed error handling.

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -31,16 +32,16 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	// Do some basic validation
 	if request.HTTPMethod != http.MethodPost {
-		return events.APIGatewayProxyResponse{StatusCode: http.StatusMethodNotAllowed},
-			errors.New("only accept POST requests")
+		log.Println(errors.New("only accept POST requests"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusMethodNotAllowed}, nil
 	}
 	if len(request.Body) < 1 {
-		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest},
-			errors.New("no content was provided in the HTTP body")
+		log.Println(errors.New("no content was provided in the HTTP body"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest}, nil
 	}
 	if request.Headers["Content-Type"] != "application/json" {
-		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest},
-			errors.New("only accepts application/json")
+		log.Println(errors.New("only accepts application/json"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest}, nil
 	}
 
 	fmt.Println(request.Body)
@@ -51,9 +52,17 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	err := json.Unmarshal([]byte(request.Body), &image)
 
 	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest},
-			errors.New("json could not be parsed")
+		log.Println(errors.New("json could not be parsed"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest}, nil
 	}
+
+	if len(strings.TrimSpace(image.Name)) == 0 || len(image.Image) == 0 {
+		log.Println(errors.New("only accepts application/json"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusBadRequest}, nil
+	}
+
+	log.Println(image)
+	log.Printf("Uploading image: %x", image)
 
 	// put the image in S3
 	// The session the S3 Uploader will use
@@ -62,21 +71,24 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// Create an uploader with the session and default options
 	uploader := s3manager.NewUploader(sess)
 
-	// Upload the file to S3.
-	result, err := uploader.Upload(&s3manager.UploadInput{
+	upload := &s3manager.UploadInput{
 		Bucket: aws.String("gritty-image-uploads"),
 		Key:    aws.String(image.Name),
 		Body:   bytes.NewReader(image.Image),
-	})
-	if err != nil {
-		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError},
-			errors.New("could not upload the file to s3")
 	}
-	fmt.Printf("file uploaded to, %s\n", aws.StringValue(&result.Location))
+
+	// Upload the file to S3.
+	result, err := uploader.Upload(upload)
+	if err != nil {
+		log.Println(errors.New("could not upload the file to s3"))
+		return events.APIGatewayProxyResponse{StatusCode: http.StatusInternalServerError}, nil
+	}
+	log.Printf("file uploaded to, %s\n", aws.StringValue(&result.Location))
 
 	// return the image's URL
 	return events.APIGatewayProxyResponse{
-		Body:       image.Name,
+		Body:       `{"url":"` + result.Location + "\"}",
+		Headers:    map[string]string{"Content-Type": "application/json"},
 		StatusCode: 200,
 	}, nil
 }
